126327b749b02bbf3e6919c03ad24cb8e0585c95
mbalcer <mateusz75319@gmail.com>
2021-05-24 21:45:53 +0200

    Added combobox for image selection

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -12,15 +12,25 @@ import utils.FileHandler;
 import utils.MyLogger;
 
 import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainController implements Initializable {
     @FXML
     private Button startBtn;
 
     @FXML
-    private ComboBox algorithmComboBox;
+    private ComboBox<AlgorithmType> algorithmComboBox;
+
+    @FXML
+    private ComboBox<Path> imageComboBox;
 
     @FXML
     private Slider neuronsSlider;
@@ -57,37 +67,40 @@ public class MainController implements Initializable {
     private int iterations = 100;
     private double learningRate = 0.1;
     private double mapRadius = 50.0;
+    private Path imageFile;
     private static final double PRECISION = 100.0;
     private static final String SEPARATOR = "\t";
     private MyLogger myLogger;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         myLogger = MyLogger.getInstance().textArea(logsArea);
         initAlgorithmCombobox();
+        initImageComboBox();
         initSliders();
         initButtons();
     }
 
     public void initButtons() {
+        startBtn.setDisable(true);
         startBtn.setOnAction(action -> {
+            startBtn.setDisable(true);
             FileHandler.makeEmptyDir("data_img");
-            String imageFile = "images/1.png";
             String imageData = "data_img/img.data";
             int frameSz = 4;
             double lrc = 1;
 
-            FileHandler.parseIMG(imageFile, imageData, SEPARATOR, frameSz);
+            FileHandler.parseIMG(imageFile.toString(), imageData, SEPARATOR, frameSz);
 
             new Thread(() -> {
                 NeuralGas ng = new NeuralGas(neurons, iterations, imageData, SEPARATOR, false, mapRadius, learningRate, lrc);
                 ng.calc();
 
                 FileHandler.writeMatrixToImage(FileHandler.readPixels(ng.destDir + ng.imgcprFile, SEPARATOR),
-                        imageFile, ng.destDir+"out.png", SEPARATOR, frameSz, "png");
+                        imageFile.toString(), ng.destDir+"out.png", SEPARATOR, frameSz, "png");
 
                 setImage(ng.destDir + "out.png");
+                startBtn.setDisable(false);
             }).start();
         });
     }
@@ -95,9 +108,26 @@ public class MainController implements Initializable {
     private void initAlgorithmCombobox() {
         AlgorithmType[] functionTypes = AlgorithmType.values();
         algorithmComboBox.setItems(FXCollections.observableArrayList(functionTypes));
-        algorithmComboBox.setValue(AlgorithmType.NEURAL_GAS.getName());
+        algorithmComboBox.setValue(AlgorithmType.NEURAL_GAS);
         algorithmComboBox.valueProperty().addListener(
-                (observable, oldValue, actualValue) -> algorithmType = (AlgorithmType) actualValue);
+                (observable, oldValue, actualValue) -> algorithmType = actualValue);
+    }
+
+    private void initImageComboBox() {
+        String imagesPath = "images/";
+        Path path = Paths.get(imagesPath);
+        List<Path> imagesList = null;
+        try {
+            imagesList = Files.list(Paths.get(imagesPath)).collect(Collectors.toList());
+            imageComboBox.setItems(FXCollections.observableArrayList(imagesList));
+            imageComboBox.valueProperty().addListener((observable, oldValue, actualValue) -> {
+                imageFile = actualValue;
+                startBtn.setDisable(false);
+            });
+        } catch (IOException e) {
+            imageComboBox.setItems(FXCollections.observableArrayList(new ArrayList()));
+        }
+
     }
 
     private void initSliders() {
